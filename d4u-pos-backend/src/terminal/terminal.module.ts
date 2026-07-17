608c7c5bb44d276e470a806551f6a161465d70ca
Imran <deziner4you@gmail.com>
2026-07-18 00:35:34 +0500

    Build website for WhatsApp icon and update URLs

--- d4u-pos-backend/src/terminal/terminal.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TerminalController } from './terminal.controller';
-import { PrismaService } from '../prisma/prisma.service';
-
-@Module({
-  controllers: [TerminalController],
-  providers: [PrismaService],
-})
-export class TerminalModule {}
